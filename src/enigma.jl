b30c45c4b010f79b8c682d2ded4fa6f54a055b8d
Tim Wheeler <timwheeleronline@gmail.com>
2016-11-06 18:17:17 +0100

    reproduced results from online emulator

--- src/enigma.jl
+++ src/enigma.jl
@@ -59,46 +59,119 @@ function get_surface_and_context(canvas_width::Int, canvas_height::Int)
     ctx = creategc(s)
     (s, ctx)
 end
-
-type Rotor
-    perm::Vector{Int}
-    rotor_position::Int
-    Rotor(perm::Vector{Int}, rotor_position::Int=0) = new(perm, rotor_position)
-end
-function encode(rotor::Rotor, a::Int)
-    n = length(rotor.perm) # number of letters
-    a₂ = a - rotor.rotor_position
-    b = rotor.perm[mod(a₂-1, n)+1]
-    Δ = b - a₂
-    mod(a + Δ - 1, n)+1
-end
-function decode(rotor::Rotor, a::Int)
-    for b in 1 : length(rotor.perm)
-        if encode(rotor, b) == a
-            return b
-        end
-    end
-    error("decode not possible")
-end
-
-function render(rotor::Rotor;
-    active_inputs::Vector{Int}=Int[],
+function get_surface_and_context(;
+    canvas_width::Int = 1000,
+    canvas_height::Int = 400,
+    background_color::Colorant = colorant"white",
     )
 
-    canvas_width, canvas_height = 1000, 400
     s, ctx = get_surface_and_context(canvas_width, canvas_height)
 
     # fill with background color
-    set_source_rgba( ctx, colorant"white")
+    set_source_rgba(ctx, background_color)
     paint(ctx)
 
-    # compute the vertices
-    margin_left = 200.0
-    margin_top = 50.0
-    sep_vert = 100.0
-    sep_horz = 200.0
+    (s, ctx)
+end
 
-    n = length(rotor.perm)
+bezier(A::VecE2, B::VecE2, t::Float64) = lerp(A, B, t)
+bezier(A::VecE2, B::VecE2, C::VecE2, t::Float64) = (1-t)^2*A + 2*(1-t)*t*B + t*t*C
+bezier(A::VecE2, B::VecE2, C::VecE2, D::VecE2, t::Float64) = (1-t)^3*A + 3*(1-t)*t*t*B + 3*(1-t)^2*t*C + t*t*t*D
+
+type Rotor
+    enshift::Vector{Int} # amount each input gets shifted
+    deshift::Vector{Int} # amount reversed inputs get shifted
+    knock_points::Vector{Int}
+    rotor_position::Int
+    function Rotor(perm::Vector{Int}, knock_points::Vector{Int}=Int[], rotor_position::Int=0)
+
+        n = length(perm)
+        enshift = Array(Int, n)
+        deshift = Array(Int, n)
+        for (i,p) in enumerate(perm)
+            enshift[i] = p - i
+            deshift[p] = i - p
+        end
+
+        rotor_position = mod(rotor_position, n)
+
+        new(enshift, deshift, knock_points, rotor_position)
+    end
+end
+Base.length(rotor::Rotor) = length(rotor.enshift)
+function tick!(rotor::Rotor)
+    rotor.rotor_position = mod(rotor.rotor_position + 1, length(rotor))
+    rotor
+end
+function encode(rotor::Rotor, a::Int)
+    n = length(rotor.enshift) # number of letters
+    a₂ = a + rotor.rotor_position
+    a₂ = mod(a₂-1, n)+1
+    b = a + rotor.enshift[a₂]
+    mod(b-1, n)+1
+
+    # a₂ = a - rotor.rotor_position
+    # b = rotor.perm[mod(a₂-1, n)+1]
+    # Δ = b - a₂
+    # mod(a + Δ - 1, n)+1
+end
+function decode(rotor::Rotor, a::Int)
+    n = length(rotor.deshift) # number of letters
+    a₂ = a + rotor.rotor_position
+    a₂ = mod(a₂-1, n)+1
+    b = a + rotor.deshift[a₂]
+    mod(b-1, n)+1
+    # for b in 1 : length(rotor.enshift)
+    #     if encode(rotor, b) == a
+    #         return b
+    #     end
+    # end
+    # error("decode not possible")
+end
+
+function render_bezier_edge!(ctx::CairoContext, A::VecE2, D::VecE2, color::Colorant=MONOKAI_DARK;
+    line_width::Real = 5.0,
+    bezier_strength::Real = 150.0,
+    reflect::Bool=false,
+    )
+
+    B = A + VecE2(bezier_strength, 0.0)
+    C = D + (reflect ? 1 : -1) * VecE2(bezier_strength, 0.0)
+
+    save(ctx)
+    set_source_rgba(ctx, color)
+    set_line_width(ctx, line_width)
+    move_to(ctx, A.x, A.y)
+    curve_to(ctx, B.x, B.y, C.x, C.y, D.x, D.y)
+    stroke(ctx)
+    restore(ctx)
+
+    ctx
+end
+function render_vertex!(ctx::CairoContext, P::VecE2, color::Colorant=MONOKAI_DARK;
+    radius::Real=10.0,
+    )
+
+    save(ctx)
+    set_source_rgba(ctx, color)
+    translate(ctx, P.x, P.y)
+    arc(ctx, 0.0, 0.0, radius, 0, 2pi)
+    fill(ctx)
+    restore(ctx)
+
+    ctx
+end
+function render!(ctx::CairoContext,
+    rotor::Rotor,
+    active_inputs::Vector{Int},
+    margin_left::Real,
+    margin_top::Real,
+    sep_vert::Real,
+    sep_horz::Real,
+    )
+
+    # compute the vertices
+    n = length(rotor.enshift)
     points_left = Array(VecE2, n)
     points_right = Array(VecE2, n)
     active_left = falses(n)
@@ -120,36 +193,261 @@ function render(rotor::Rotor;
     for (points, active) in zip((points_left, points_right),
                                 (active_left, active_right))
         for (i, pt) in enumerate(points)
-            save(ctx)
-            set_source_rgba(ctx, active[i] ? MONOKAI_COBALT : MONOKAI_DARK)
-            translate(ctx, pt.x, pt.y)
-            arc(ctx, 0.0, 0.0, radius, 0, 2pi)
-            fill(ctx)
-            restore(ctx)
+            render_vertex!(ctx, pt, active_left[i] ? MONOKAI_COBALT : MONOKAI_DARK)
         end
     end
     restore(ctx)
 
     # render the connections
-    line_width = 5.0
-    bezier_strength = 150.0
     save(ctx)
-    set_line_width(ctx, line_width)
-    set_source_rgba(ctx, MONOKAI_DARK)
     for i in 1 : n
         A, D = points_left[i], points_right[i]
-
-        B = A + VecE2(bezier_strength, 0.0)
-        C = D - VecE2(bezier_strength, 0.0)
-
-        save(ctx)
-        set_source_rgba(ctx, active_left[i] ? MONOKAI_COBALT : MONOKAI_DARK)
-        move_to(ctx, A.x, A.y)
-        curve_to(ctx, B.x, B.y, C.x, C.y, D.x, D.y)
-        stroke(ctx)
-        restore(ctx)
+        render_bezier_edge!(ctx, A, D, active_left[i] ? MONOKAI_COBALT : MONOKAI_DARK)
     end
     restore(ctx)
 
+    ctx
+end
+function render(rotor::Rotor;
+    active_inputs::Vector{Int}=Int[],
+    margin_left::Float64 = 200.0,
+    margin_top::Float64 = 50.0,
+    sep_vert::Float64 = 100.0,
+    sep_horz::Float64 = 200.0,
+    )
+
+    s, ctx = get_surface_and_context()
+    render!(ctx, rotor, active_inputs, margin_left, margin_top, sep_vert, sep_horz)
     s
 end
+
+let
+    rotor = Rotor([1, 2, 3, 4])
+    for i in 1 : 4
+        @assert encode(rotor, i) == i
+        @assert decode(rotor, i) == i
+    end
+
+    tick!(rotor)
+    for i in 1 : 4
+        @assert encode(rotor, i) == i
+        @assert decode(rotor, i) == i
+    end
+
+    rotor = Rotor([2, 1, 4, 3]) #+1  -1  +1  -1#
+    @assert encode(rotor, 1) == 2 # +1
+    @assert encode(rotor, 2) == 1 # -1
+    @assert encode(rotor, 3) == 4 # +1
+    @assert encode(rotor, 4) == 3 # -1
+
+    @assert decode(rotor, 1) == 2
+    @assert decode(rotor, 2) == 1
+    @assert decode(rotor, 3) == 4
+    @assert decode(rotor, 4) == 3
+
+    tick!(rotor)
+    @assert encode(rotor, 1) == 4 # (1+1 → 2) -1
+    @assert encode(rotor, 2) == 3 # (2+1 → 3) +1
+    @assert encode(rotor, 3) == 2 # (3+1 → 4) -1
+    @assert encode(rotor, 4) == 1 # (4+1 → 1) +1
+
+    @assert decode(rotor, 1) == 4
+    @assert decode(rotor, 2) == 3
+    @assert decode(rotor, 3) == 2
+    @assert decode(rotor, 4) == 1
+
+    rotor = Rotor([4, 2, 1, 3])
+    @assert encode(rotor, 1) == 4
+    @assert encode(rotor, 2) == 2
+    @assert encode(rotor, 3) == 1
+    @assert encode(rotor, 4) == 3
+
+    @assert decode(rotor, 1) == 3
+    @assert decode(rotor, 2) == 2
+    @assert decode(rotor, 3) == 4
+    @assert decode(rotor, 4) == 1
+end
+
+function encode(rotors::Vector{Rotor}, a::Int)
+    for rotor in rotors
+        a = encode(rotor, a)
+    end
+    a
+end
+function decode(rotors::Vector{Rotor}, a::Int)
+    for rotor in reverse(rotors)
+        a = decode(rotor, a)
+    end
+    a
+end
+function render(rotors::Vector{Rotor};
+    margin_top::Float64 = 50.0,
+    sep_vert::Float64 = 100.0,
+    sep_horz::Float64 = 200.0,
+    )
+
+    s, ctx = get_surface_and_context()
+
+    for (i,rotor) in enumerate(rotors)
+        margin_left = 200.0*i
+        render!(ctx, rotor, Int[], margin_left, margin_top, sep_vert, sep_horz)
+    end
+
+    s
+end
+
+let
+    rotor = Rotor([4, 2, 1, 3])
+    rotors = [rotor, rotor, rotor]
+    @assert encode(rotors, 1) == 1
+    @assert encode(rotors, 2) == 2
+    @assert encode(rotors, 3) == 3
+    @assert encode(rotors, 4) == 4
+    @assert decode(rotors, 1) == 1
+    @assert decode(rotors, 2) == 2
+    @assert decode(rotors, 3) == 3
+    @assert decode(rotors, 4) == 4
+end
+
+type Reflector
+    perm::Vector{Int} # pairwise combinations, so perm[a] = b and perm[b] = a, a ≠ b
+    function Reflector(perm::Vector{Int})
+        for (i,p) in enumerate(perm)
+            @assert perm[p] == i && i != p
+        end
+        new(perm)
+    end
+end
+Base.length(ref::Reflector) = length(ref.perm)
+encode(ref::Reflector, a::Int) = ref.perm[a]
+decode(ref::Reflector, b::Int) = ref.perm[b]
+
+let
+    ref = Reflector([2,1,4,3])
+    @assert encode(ref, 1) == 2
+    @assert encode(ref, 2) == 1
+    @assert encode(ref, 3) == 4
+    @assert encode(ref, 4) == 3
+
+    @assert decode(ref, 1) == 2
+    @assert decode(ref, 2) == 1
+    @assert decode(ref, 3) == 4
+    @assert decode(ref, 4) == 3
+end
+
+type Plugboard
+    perm::Vector{Int} # optional pairwise perms
+end
+Base.length(plugboard::Plugboard) = length(plugboard.perm)
+encode(plugboard::Plugboard, a::Int) = plugboard.perm[a]
+decode(plugboard::Plugboard, b::Int) = plugboard.perm[b]
+
+type Enigma
+    plugboard::Plugboard
+    rotors::Vector{Rotor}
+    ref::Reflector
+end
+Base.length(enigma::Enigma) = length(enigma.ref)
+function encode(enigma::Enigma, a::Int)
+    b = encode(enigma.rotors, a)
+    c = encode(enigma.ref, b)
+    decode(enigma.rotors, c)
+end
+decode(enigma::Enigma, b::Int) = encode(enigma, b)
+function tick!(enigma::Enigma, rotor_index::Int=1)
+    rotor = enigma.rotors[rotor_index]
+
+    if mod(rotor.rotor_position,26)+1 in rotor.knock_points && rotor_index < length(enigma.rotors)
+        # rotate the next rotor as well
+        tick!(enigma, rotor_index+1)
+    end
+    tick!(rotor) # rotate this rotor
+    enigma
+end
+
+function encode!(enigma::Enigma, a::Int)
+    #=
+    Enigma Process
+         1. Rotate wheels
+         2. Encode
+    =#
+
+    tick!(enigma)
+    b = encode(enigma, a)
+
+    b
+end
+
+function render_trace!(
+    ctx::CairoContext,
+    enigma::Enigma,
+    a::Int,
+    color::Colorant,
+    margin_left::Real,
+    margin_top::Real,
+    sep_vert::Real,
+    sep_horz::Real,
+    )
+
+    P = VecE2(margin_left, margin_top + sep_vert*(a-1))
+    render_vertex!(ctx, P, color)
+
+    for rotor in enigma.rotors
+        b = encode(rotor, a)
+        Q = VecE2(P.x + sep_horz, margin_top + sep_vert*(b-1))
+        render_bezier_edge!(ctx, P, Q, color)
+        render_vertex!(ctx, Q, color)
+        a, P = b, Q
+    end
+
+    b = encode(enigma.ref, a)
+    Q = VecE2(P.x, margin_top + sep_vert*(b-1))
+    render_bezier_edge!(ctx, Q, P, color, reflect=true)
+    render_vertex!(ctx, Q, color)
+    a, P = b, Q
+
+    for rotor in reverse(enigma.rotors)
+        b = decode(rotor, a)
+        Q = VecE2(P.x - sep_horz, margin_top + sep_vert*(b-1))
+        render_bezier_edge!(ctx, Q, P, color)
+        render_vertex!(ctx, Q, color)
+        a, P = b, Q
+    end
+
+    ctx
+end
+function render!(
+    ctx::CairoContext,
+    enigma::Enigma,
+    input::Int,
+    margin_left::Real,
+    margin_top::Real,
+    sep_vert::Real,
+    sep_horz::Real,
+    )
+
+    rendered = Set{Int}()
+    for a in 1 : length(enigma)
+        if !in(a, rendered)
+            b = encode(enigma, a)
+            push!(rendered, b)
+            color = a == input || b == input ? MONOKAI_BLUE : MONOKAI_DARK
+            render_trace!(ctx, enigma, a, color, margin_left, margin_top, sep_vert, sep_horz)
+        end
+    end
+
+    ctx
+end
+function render(enigma::Enigma, input::Int=-1;
+    margin_left::Float64 = 200.0,
+    margin_top::Float64 = 50.0,
+    sep_vert::Float64 = 100.0,
+    sep_horz::Float64 = 200.0,
+    )
+
+    s, ctx = get_surface_and_context()
+    render!(ctx, enigma, input, margin_left, margin_top, sep_vert, sep_horz)
+    s
+end
+
+include("fullsize.jl")
---
+++ src/fullsize.jl
@@ -0,0 +1,91 @@
+char2ind(c::Char) = uppercase(c) - 'A' + 1
+ind2char(i::Int) = i + 'A' - 1
+function get_perm(str::String)
+    p = Array(Int, length(str))
+    for (i,c) in enumerate(str)
+        p[i] = char2ind(c)
+    end
+    p
+end
+
+const ENIGMA_ROTORS = [
+    Rotor(get_perm("EKMFLGDQVZNTOWYHXUSPAIBRCJ"), Int[char2ind('Q')]), # I
+    Rotor(get_perm("AJDKSIRUXBLHWTMCQGZNPYFVOE"), Int[char2ind('E')]), # II
+    Rotor(get_perm("BDFHJLCPRTXVZNYEIWGAKMUSQO"), Int[char2ind('V')]), # III
+    Rotor(get_perm("ESOVPZJAYQUIRHXLNFTGKDCMWB"), Int[char2ind('J')]), # IV
+    Rotor(get_perm("VZBRGITYUPSDNHLXAWMJQOFECK"), Int[char2ind('Z')]), # V
+    Rotor(get_perm("JPGVOUMFYQBENHZRDKASXLICTW"), Int[char2ind('Z'), char2ind('M')]), # VI
+    Rotor(get_perm("NZJHGRCXMYSWBOUFAIVLPEKQDT"), Int[char2ind('Z'), char2ind('M')]), # VII
+    Rotor(get_perm("FKQHTLXOCBJSPDZRAMEWNIUYGV"), Int[char2ind('Z'), char2ind('M')]), # VIII
+]
+
+const ENIGMA_REFLECTORS = [
+    Reflector(get_perm("YRUHQSLDPXNGOKMIEBFZCWVJAT")), # b
+    Reflector(get_perm("FVPJIAOYEDRZXWGCTKUQSBNMHL")), # c
+    Reflector(get_perm("ENKQAUYWJICOPBLMDXZVFTHRGS")), # b - thin
+    Reflector(get_perm("RDOBJNTKVEHMLFCWZAXGYIPSUQ")), # c - thin
+]
+
+function get_M3_enigma(
+    ref::Char, # 'B' or 'C'
+    wheels::Tuple{Int, Int, Int},
+    rotor_pos::String, # the starting rotor positions, ex: "DVM"
+    plugboard_str::String="", # ex: "BI GP HK LD MO RT VS WZ XN YJ"
+    )
+
+    if ref == 'B'
+        reflector = ENIGMA_REFLECTORS[1]
+    elseif ref == 'C'
+        reflector = ENIGMA_REFLECTORS[2]
+    else
+        error("unknown reflector $ref")
+    end
+
+    rotors = ENIGMA_ROTORS[[wheels[3], wheels[2], wheels[1]]]
+    rotors[1].rotor_position = rotor_pos[3]-'A'
+    rotors[2].rotor_position = rotor_pos[2]-'A'
+    rotors[3].rotor_position = rotor_pos[1]-'A'
+
+    plugboard = Plugboard(collect(1:26))
+    plugboard_str = replace(plugboard_str, " ", "")
+    i = 1
+    while i < length(plugboard_str)
+        a = char2ind(plugboard_str[i])
+        b = char2ind(plugboard_str[i+1])
+        plugboard.perm[a] = b
+        plugboard.perm[b] = a
+        i += 2
+    end
+
+    Enigma(plugboard, rotors, reflector)
+end
+
+function encode!(enigma::Enigma, input::String)
+    output = ""
+    for c in input
+        output = output * string(ind2char(encode!(enigma, char2ind(c))))
+    end
+    output
+end
+
+let
+    enigma = get_M3_enigma('B', (1,2,3), "AAA")
+    input =  "THEQ"*"UICK"*"BROW"*"NFOX"*"JUMP"*"EDOV"*"ERTH"*"ELAZ"*"YDOG"
+    output = "OPCI"*"LLAZ"*"FXLQ"*"TDNL"*"GGLE"*"YOHH"*"CJGA"*"XWTW"*"AMBH"
+    @assert encode!(enigma, input) == output
+
+    enigma = get_M3_enigma('B', (1,2,3), "AAA")
+    @assert encode!(enigma, output) == input
+
+    enigma = get_M3_enigma('B', (3,5,8), "MCJ")
+    output2 = "JFHJ"*"BXOD"*"IIZK"*"CHJU"*"TGOL"*"FIYX"*"RSLF"*"KKPO"*"JVRO"
+    @assert encode!(enigma, input) == output2
+    enigma = get_M3_enigma('B', (3,5,8), "MCJ")
+    @assert encode!(enigma, output2) == input
+
+    enigma = get_M3_enigma('C', (8,2,4), "PXM", "AB CY ET FQ HZ LG MJ OK US WX")
+    output3 = "GXUH"*"KCKB"*"QJKL"*"QJUZ"*"SFNU"*"BQBX"*"HFYM"*"DIRE"*"PIRN"
+    @assert encode!(enigma, input) == output3
+    enigma = get_M3_enigma('C', (8,2,4), "PXM", "AB CY ET FQ HZ LG MJ OK US WX")
+    @assert encode!(enigma, output3) == input
+end
